2d7e2d2d90d902812ce3bc47fd61b92a45609042
Kinnari shah <kinnari.shah@kiwiqa.com>
2017-07-07 17:06:07 +0530

    Kleenex Project On GIt for Pull

---
+++ Kleenex/Resource/hub.bat
@@ -0,0 +1,6 @@
+cd..
+cd..
+cd..
+java -jar selenium-server-standalone-2.53.1.jar -role hub
+pause
+
---
+++ Kleenex/Resource/node.bat
@@ -0,0 +1,5 @@
+cd..
+cd..
+cd..
+java -jar selenium-server-standalone-2.53.1.jar -role node -port 8998 -Dwebdriver.chrome.driver="E:\chromedriver.exe"
+pause
